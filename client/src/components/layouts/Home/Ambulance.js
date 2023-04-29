876dfe8bdc87b991daffb9e34026531a5bf31137
Harshit <7harshit20@gmail.com>
2023-04-29 22:19:45 +0530

    updated ambulance page

--- client/src/components/layouts/Home/Ambulance.js
+++ client/src/components/layouts/Home/Ambulance.js
@@ -1,15 +1,50 @@
-import React, { useEffect, useContext } from "react";
-import GlobalContext from "../../../context/global/GlobalContext";
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "../../../CSSFiles/Ambulance.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-VUgp_g0k6Vo1WAgpJfF_tz74HL0J9QI",
+  authDomain: "gps-data-demo.firebaseapp.com",
+  databaseURL:
+    "https://gps-data-demo-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gps-data-demo",
+  storageBucket: "gps-data-demo.appspot.com",
+  messagingSenderId: "778424700278",
+  appId: "1:778424700278:web:ac8c0537f5b41863b97e57",
+  // Your Firebase config goes here
+};
+firebase.initializeApp(firebaseConfig);
 
 const Ambulance = () => {
-  const globalContext = useContext(GlobalContext);
-  const { latitude, longitude, fetchLocation } = globalContext;
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-    fetchLocation();
-  });
+    const databaseRef = firebase.database().ref();
+    const locationRef = databaseRef.child(
+      "UsersData/rPyQOFDu9zVLGdWiA7tJZ7cs6wX2/readings"
+    );
+    //console.log('Database ref:', databaseRef.toString());
+    //console.log('Location ref:', locationRef.toString());
+    locationRef.once("value", (snapshot) => {
+      const locationsData = snapshot.val();
+      const locationsArray = Object.entries(locationsData).map(
+        ([timestamp, location]) => ({
+          latitude: location.latitude,
+          longitude: location.longitude,
+        })
+      );
+      setLocations(locationsArray);
+    });
+  }, []);
 
   useEffect(() => {
+    const lastLocation =
+      locations.length > 0 ? locations[locations.length - 1] : null; // get the last element of the array
+    const latitude = lastLocation?.latitude;
+    const longitude = lastLocation?.longitude;
+    console.log(latitude, longitude);
+    if (!latitude || !longitude) return;
     const script = document.createElement("script");
     script.src =
       "https://apis.mappls.com/advancedmaps/api/802271a9e6629027f398d0716bb07bc9/map_sdk?layer=vector&v=3.0&callback=initMap1";
@@ -39,9 +74,53 @@ const Ambulance = () => {
     return () => {
       document.head.removeChild(script);
     };
-  }, [latitude, longitude]);
+  }, [locations]);
 
-  return <div id='map' style={{ width: "100%", height: "100vh" }}></div>;
+  // const t = new Date();
+  // //console.log(t);
+  const lastLocation =
+    locations.length > 0 ? locations[locations.length - 1] : null; // get the last element of the array
+  const lati = lastLocation?.latitude;
+  const longi = lastLocation?.longitude;
+  const url =
+    "http://maps.google.com/maps?&z=15&mrt=yp&t=k&q=" + lati + "+" + longi;
+  //console.log(url);
+  return (
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     border: "",
+    //     justifyContent: "center",
+    //     margin: "auto",
+    //     flexDirection: "column",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   {lastLocation ? ( // display the last location if it exists
+    //     <p style={{ fontSize: "32px", fontWeight: "900" }}>
+    //       Last location: Latitude: {lastLocation.latitude}, Longitude:{" "}
+    //       {lastLocation.longitude}
+    //     </p>
+    //   ) : (
+    //     <p>Loading please wait...</p>
+    //   )}
+    //   <p>{t.toString()}</p>
+    //   {/* <Link to={url}>See on Google Maps</Link> */}
+    // <a target='_blank' href={url}>
+    //   See on Google Maps
+    // </a>
+    // </div>
+    <>
+      <nav class='navbar navbar-primary '>
+        <div class='container-fluid'>
+          <a class='navbar-brand' target='_blank' href={url}>
+            See on Google Maps
+          </a>
+        </div>
+      </nav>
+      <div id='map' style={{ width: "100%", height: "100vh" }}></div>
+    </>
+  );
 };
 
 export default Ambulance;
